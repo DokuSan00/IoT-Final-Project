ecfea5450fced67b96b525e42f76645460939a6a
DokuSan00 <kanekike12@gmail.com>
2023-09-28 16:21:33 -0400

    finished

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import RPi.GPIO as GPIO
-
-from flask import Flask, render_template, request, url_for, flash, redirect
-app = Flask(__name__)
+import time as time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(26, GPIO.OUT, initial=0)
+LED = 26
+
+GPIO.setup(LED, GPIO.OUT, initial=0)
+
+from flask import Flask, render_template, request, url_for, flash, redirect
+app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
@@ -14,9 +17,9 @@ def index():
 
 @app.route("/toggle_light", methods=["POST"])
 def toggle_light():
-    # path = request.form['projectFilePath']
-    GPIO.output(26, not(bool(GPIO.input(26))))
+    time.sleep(0.1)
+    GPIO.output(LED, not(bool(GPIO.input(LED))))  #on off the light
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
--- static/scripts/light.js
+++ static/scripts/light.js
@@ -1,3 +1,12 @@
 function toggleLight() {
-    $.post('toggle_light');
+    var e = ".light-toggle-div";
+    $(e).toggleClass("off");
+    $(e).toggleClass("on");
+
+    if ($(e).hasClass("on"))
+        $("#light-toggle-btn").html("Light On");
+    if ($(e).hasClass("off"))
+        $("#light-toggle-btn").html("Light Off");
+        
+        $.post("/toggle_light");
 }
